76eb11dfaebe236bbd95b29dcc573465daeb3683
Andreas Postl <42180854+apostl88@users.noreply.github.com>
2024-05-14 23:06:13 +0200

    skeleton

---
+++ day10.py
@@ -0,0 +1,14 @@
+import re
+
+with open("day10_example.txt", "r") as f:
+    puzzle_input = f.read()
+
+def part1(puzzle_input):
+    pass
+
+print(part1(puzzle_input))
+
+def part2(puzzle_input):
+    pass
+
+print(part2(puzzle_input))
