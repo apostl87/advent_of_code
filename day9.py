bdf6f652834c18a7eb5473d111964746c0e81bbe
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-05-14 22:04:47 +0200

    dev (#2)
    * simplified summation of final_numbers
    
    * put code into a function part1()
    
    * Day 9 Part 2 correct
    
    * test
    
    * test
    
    ---------
    
    Co-authored-by: Andreas Postl <42180854+apostl88@users.noreply.github.com>

--- day9.py
+++ day9.py
@@ -3,19 +3,34 @@ import re
 with open("day9_input.txt", "r") as f:
     puzzle_input = f.read()
 
-histories = puzzle_input.split("\n")
+def part1(puzzle_input):
+    histories = puzzle_input.split("\n")
+    total = 0
+    for history in histories:
+        numbers = [int(x) for x in history.split()]
+        final_numbers = []
+        while set(numbers) != set([0]):
+            final_numbers.append(numbers[-1])
+            numbers = [numbers[i+1]-numbers[i] for i in range(len(numbers)-1)]
+        total += sum(final_numbers)
+    return total
 
-total = 0
-for history in histories:
-    numbers = [int(x) for x in history.split()]
-    final_numbers = []
-    while set(numbers) != set([0]):
-        final_numbers.append(numbers[-1])
-        numbers = [numbers[i+1]-numbers[i] for i in range(len(numbers)-1)]
-    result = final_numbers.pop()
-    while final_numbers:
-        final_numbers[-1] += result
-        result = final_numbers.pop()
-    total += result
+print(part1(puzzle_input))
 
-print(total)
+def part2(puzzle_input):
+    histories = puzzle_input.split("\n")
+    total = 0
+    for history in histories:
+        numbers = [int(x) for x in history.split()]
+        initial_numbers = []
+        while set(numbers) != set([0]):
+            initial_numbers.append(numbers[0])
+            numbers = [numbers[i+1]-numbers[i] for i in range(len(numbers)-1)]
+        while len(initial_numbers)>1:
+            sub = initial_numbers.pop()
+            initial_numbers[-1] -= sub
+        total += initial_numbers[0]
+    return total
+
+print(part2(puzzle_input))
+
