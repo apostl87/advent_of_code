2b2f0bbdf25aa323012ac354b115b7e3ecc13cbf
Andreas Postl <42180854+apostl88@users.noreply.github.com>
2024-05-14 21:23:44 +0200

    put code into a function part1()

--- day9.py
+++ day9.py
@@ -3,15 +3,16 @@ import re
 with open("day9_input.txt", "r") as f:
     puzzle_input = f.read()
 
-histories = puzzle_input.split("\n")
+def part1(puzzle_input):
+    histories = puzzle_input.split("\n")
+    total = 0
+    for history in histories:
+        numbers = [int(x) for x in history.split()]
+        final_numbers = []
+        while set(numbers) != set([0]):
+            final_numbers.append(numbers[-1])
+            numbers = [numbers[i+1]-numbers[i] for i in range(len(numbers)-1)]
+        total += sum(final_numbers)
 
-total = 0
-for history in histories:
-    numbers = [int(x) for x in history.split()]
-    final_numbers = []
-    while set(numbers) != set([0]):
-        final_numbers.append(numbers[-1])
-        numbers = [numbers[i+1]-numbers[i] for i in range(len(numbers)-1)]
-    total += sum(final_numbers)
+print(part1(puzzle_input))
 
-print(total)
